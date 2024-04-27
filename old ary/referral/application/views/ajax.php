b8cf363cce5e0cdfa30e853a6cc80155fd4d2fe4
rhlrjn2202@gmail.com <rhlrjn2202@gmail.com>
2024-04-27 13:01:24 +0530

    com4

--- old ary/referral/application/views/ajax.php
+++
@@ -1,7 +0,0 @@
-<?php $db= new mysqli('localhost','utqkdsjgz1ezi','dxrdkvr6rsvl','dbzop7x90qlkp4'); 
-extract($_POST);
-$nodue_id=$db->real_escape_string($nodue_id);
-$status=$db->real_escape_string($status);
-$sql=$db->query("UPDATE nodue_application SET status='$status' WHERE nodue_id='$nodue_id'");
-echo 1;
-?>
